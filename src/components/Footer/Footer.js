8c17a101cc5d24cf2dda5b6752f7ae9791007228
SatoruSaito <satorusaito1108@gmail.com>
2024-12-24 10:43:06 -0800

    Firebase Env Updated again

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,7 +15,9 @@ const Footer = () => {
             style={{ backgroundColor: "#ffffff", color: "#679cc1" }}
           >
             <div>
-              <span style={{ marginRight: "10px" }}>Get connected with us on social networks :  </span>
+              <span style={{ marginRight: "10px" }}>
+                Get connected with us on social networks :{" "}
+              </span>
 
               <a href="/" className="text-white me-4">
                 <FaFacebookF style={{ color: "#679cc1" }} />
@@ -27,95 +29,154 @@ const Footer = () => {
                 <FaInstagram style={{ color: "#679cc1" }} />
               </a>
             </div>
-
           </div>
 
           <section className="">
             <div className="container text-center text-md-start mt-5">
-
               <div className="row mt-3">
-
                 <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-
                   <h6 className="text-uppercase fw-bold">Holiday Dreams</h6>
                   <hr
                     className="mb-4 mt-0 d-inline-block mx-auto"
-                    style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }}
+                    style={{
+                      width: "60px",
+                      backgroundColor: "#7c4dff",
+                      height: "2px",
+                    }}
                   />
                   <p>
-                    You can regularly visit our website for Exciting Updates and Packages . To avail more , you can register to our website .
+                    You can regularly visit our website for Exciting Updates and
+                    Packages . To avail more , you can register to our website .
                   </p>
                 </div>
 
                 <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-
                   <h6 className="text-uppercase fw-bold">Packages</h6>
                   <hr
                     className="mb-4 mt-0 d-inline-block mx-auto"
-                    style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }}
+                    style={{
+                      width: "60px",
+                      backgroundColor: "#7c4dff",
+                      height: "2px",
+                    }}
                   />
                   <p>
-                    <a href="/" className="text-white" style={{ textDecoration: "none" }}>Honeymoon</a>
+                    <a
+                      href="/"
+                      className="text-white"
+                      style={{ textDecoration: "none" }}
+                    >
+                      Honeymoon
+                    </a>
                   </p>
                   <p>
-                    <a href="/" className="text-white" style={{ textDecoration: "none" }}>Group</a>
+                    <a
+                      href="/"
+                      className="text-white"
+                      style={{ textDecoration: "none" }}
+                    >
+                      Group
+                    </a>
                   </p>
                   <p>
-                    <a href="/" className="text-white" style={{ textDecoration: "none" }}>Corporate</a>
+                    <a
+                      href="/"
+                      className="text-white"
+                      style={{ textDecoration: "none" }}
+                    >
+                      Corporate
+                    </a>
                   </p>
                   <p>
-                    <a href="/" className="text-white" style={{ textDecoration: "none" }}>Weekly Based</a>
+                    <a
+                      href="/"
+                      className="text-white"
+                      style={{ textDecoration: "none" }}
+                    >
+                      Weekly Based
+                    </a>
                   </p>
                 </div>
 
                 <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-
                   <h6 className="text-uppercase fw-bold">Useful links</h6>
                   <hr
                     className="mb-4 mt-0 d-inline-block mx-auto"
-                    style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }}
+                    style={{
+                      width: "60px",
+                      backgroundColor: "#7c4dff",
+                      height: "2px",
+                    }}
                   />
                   <p>
-                    <a href="/" className="text-white" style={{ textDecoration: "none" }}>Your Account</a>
+                    <a
+                      href="/"
+                      className="text-white"
+                      style={{ textDecoration: "none" }}
+                    >
+                      Your Account
+                    </a>
                   </p>
                   <p>
-                    <a href="/" className="text-white" style={{ textDecoration: "none" }}>Become an Affiliate</a>
+                    <a
+                      href="/"
+                      className="text-white"
+                      style={{ textDecoration: "none" }}
+                    >
+                      Become an Affiliate
+                    </a>
                   </p>
                   <p>
-                    <a href="/" className="text-white" style={{ textDecoration: "none" }}>Travel Package</a>
+                    <a
+                      href="/"
+                      className="text-white"
+                      style={{ textDecoration: "none" }}
+                    >
+                      Travel Package
+                    </a>
                   </p>
                   <p>
-                    <a href="/" className="text-white" style={{ textDecoration: "none" }}>Help</a>
+                    <a
+                      href="/"
+                      className="text-white"
+                      style={{ textDecoration: "none" }}
+                    >
+                      Help
+                    </a>
                   </p>
                 </div>
 
-
-
                 <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-40">
-
                   <h6 className="text-uppercase fw-bold">Contact</h6>
                   <hr
                     className="mb-4 mt-0 d-inline-block mx-auto"
-                    style={{ width: "60px", backgroundColor: "#7c4dff", height: "2px" }}
+                    style={{
+                      width: "60px",
+                      backgroundColor: "#7c4dff",
+                      height: "2px",
+                    }}
                   />
-                  <p><i className="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                  <p><i className="fas fa-envelope mr-3"></i> info@example.com</p>
-                  <p><i className="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                  <p><i className="fas fa-print mr-3"></i> + 01 234 567 89</p>
+                  <p>
+                    <i className="fas fa-home mr-3"></i> New York, NY 10012, US
+                  </p>
+                  <p>
+                    <i className="fas fa-envelope mr-3"></i> info@example.com
+                  </p>
+                  <p>
+                    <i className="fas fa-phone mr-3"></i> + 01 234 567 88
+                  </p>
+                  <p>
+                    <i className="fas fa-print mr-3"></i> + 01 234 567 89
+                  </p>
                 </div>
-
               </div>
-
             </div>
           </section>
-
         </div>
-
-
       </div>
 
       <p className={classes.copyright}>
-        Copyright © 2022 All rights reserved | Holiday Dreams
+        Copyright © 2016 All rights reserved | Holiday Dreams
       </p>
     </footer>
   );
